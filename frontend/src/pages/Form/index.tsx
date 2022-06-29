3b4929da075f40654ce6313a3118699ade08991c
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-06-29 13:32:57 -0300

    form

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,14 @@
+import FormCard from 'components/FormCard';
+import { useParams } from 'react-router-dom';
+
+
+function Form() {
+
+    const params = useParams();
+
+    return (
+        <FormCard movieId={`${params.movieId}`} />
+    );
+}
+
+export default Form;
