d0aca11d9fac2a1053df423a1cdbdd8ef6315d8a
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-07-03 16:58:29 -0300

    navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/AnjosClaudia" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/anjosclaudia</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
