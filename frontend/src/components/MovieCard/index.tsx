0781c44439fe3e389956f00b1a1e049ead2d12be
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-07-03 16:57:01 -0300

    moviecard

---
+++ frontend/src/components/MovieCard/index.tsx
@@ -0,0 +1,28 @@
+import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
+
+type Props = {
+    movie: Movie;
+}
+
+function MovieCard({ movie }: Props) {
+
+    return (
+        <div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+
+                <MovieScore count={movie.count} score={movie.score} />
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
