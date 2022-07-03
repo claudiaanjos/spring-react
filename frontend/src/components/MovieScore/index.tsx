86cc17d92cfbb26961ab4d2c8cdce3154d650f3a
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-07-03 16:57:28 -0300

    moviescore

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,20 @@
+import MovieStars from 'components/MovieStars';
+import './styles.css';
+
+type Props = {
+    score: number;
+    count: number
+}
+
+function MovieScore({ score, count }: Props) {
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars score={score} />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
+
+export default MovieScore;
